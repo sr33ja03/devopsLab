b56fe397c690f1977125d30b9dd65c907d94b998
Glauber Gallego <glauber.gallego@gmail.com>
2016-08-15 21:24:50 -0300

    LAB: revert commit broken code

--- src/main/java/com/amazonaws/labs/sampleapp/MvcConfiguration.java
+++ src/main/java/com/amazonaws/labs/sampleapp/MvcConfiguration.java
@@ -1,7 +1,5 @@
 package com.amazonaws.labs.sampleapp;
 
-<< BROKEN CODE HERE >>
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
